73e9af475dae0f1d153cc667f050d8ebb2bb02a0
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-07-19 15:45:02 +0530

    Added link to the maze challenge

--- 6-escaping_the_maze.py
+++ 6-escaping_the_maze.py
@@ -1,1 +1,4 @@
+# Link to the challenge
+# Reboorg's world
+# Link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
