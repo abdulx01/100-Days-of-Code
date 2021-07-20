6fe600559da289b4dfe82d7c169ea28eb9d1ff5c
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-07-20 15:31:57 +0530

    Updated file with steps to be followed

--- 7-hangman.py
+++ 7-hangman.py
@@ -1,1 +1,5 @@
-
+# STEPS
+# 1. Choose a word at random, read in the user's guess and check for a match
+# 2. Replacing blanks with guesses
+# 3. Check if player has won
+# 4. Keep track of player's lives
