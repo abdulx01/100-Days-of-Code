7e3d669e7a36e253f05f0eb72c58a2741420863d
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-07-22 15:27:39 +0530

    Update 8-caesar_cipher.py

--- 8-caesar_cipher.py
+++ 8-caesar_cipher.py
@@ -1,1 +1,62 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+# define the caesar cipher function
+def caesar(start_text, shift_amount, cipher_direction):
+  # create an empty string for the end text
+  end_text = ""
+  # As encoding => positive shift and
+  # As decoding => negative shift, multiply shift_amount by -1 when decoding
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    # ensure that we shift only letters and not spaces, numbers, symbols etc.
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+# some cool ASCII art
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+# Ask the user if they want to restart the cipher program.
+# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again.
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  # the program continues to work even if the user enters a shift greater than 26. 
+ 
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye! Have a nice day!")
+    
