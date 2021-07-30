d3c728262422f848c0e3c41b28e878c4f8ddd1c0
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-07-30 16:04:33 +0530

    Define deal_cards() function

--- 11-blackjack.py
+++ 11-blackjack.py
@@ -1,1 +1,12 @@
+import random
+
+def deal_card():
+  # cards - ACE (11), number cards (face value) and face cards (10 pts each) 
+  cards = [11,1,2,3,4,5,6,7,8,9,10,10,10,10]
+  return random.choice(cards)
+
+
+  
+
+
 
