a14469e3b2430b187645bf307a135b59f401f537
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-07-30 16:08:06 +0530

    Update 11-blackjack.py

--- 11-blackjack.py
+++ 11-blackjack.py
@@ -5,6 +5,15 @@ def deal_card():
   cards = [11,1,2,3,4,5,6,7,8,9,10,10,10,10]
   return random.choice(cards)
 
+# define lists to hold cards drawn
+user_cards = []
+comp_cards = []
+
+# draw 2 cards at random initially
+for _ in range(2):
+  user_cards.append(deal_card())
+  comp_cards.append(deal_card())
+  
 
   
 
