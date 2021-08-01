45f8441c5a4b895bbb9c7963b10483a2ce162dbc
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-08-01 16:14:46 +0530

    Added function check_answer

--- 12-guess_the_number.py
+++ 12-guess_the_number.py
@@ -1,1 +1,13 @@
+from random import randint
 
+
+def check_answer(guess,answer,turns):
+  """ Checks the user's guess against the answer. Returns the number of attempts remaining"""
+  if guess < answer:
+    print("Too low")
+    return turns-1
+  elif guess > answer:
+    print("Too high")
+    return turns-1
+  else:
+    print(f"You got it! The number was {answer}")
