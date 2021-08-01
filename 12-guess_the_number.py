ab153d5d5389ea5d23575605dff253ab7eaab365
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-08-01 16:24:57 +0530

    Added function  num_guessing_game()

--- 12-guess_the_number.py
+++ 12-guess_the_number.py
@@ -22,3 +22,26 @@ def set_difficulty():
   else:
     return HARD_TURNS
         
+def num_guessing_game():
+  
+  # Choose a random number between 1 and 100.
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+  answer = randint(1, 100)
+ 
+  turns = set_difficulty()
+  # Repeat the guessing functionality if they get it wrong and still have attempts less
+  guess = 0
+  while guess != answer:
+    print(f"You have {turns} attempts remaining to guess the number.")
+    
+    guess = int(input("Make a guess: "))
+
+    turns = check_answer(guess, answer, turns)
+    if turns == 0:
+      print("You've run out of guesses, you lose.")
+      return
+    elif guess != answer:
+      print("Guess again.")
+
+num_guessing_game()
