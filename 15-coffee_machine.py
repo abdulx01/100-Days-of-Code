7acfe18663443679493475813165ed658373707c
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-08-07 12:52:40 +0530

    Added helper fucntions
    Helper functions to check if the payment succeeds, and to deduct resources accordingly.

--- 15-coffee_machine.py
+++ 15-coffee_machine.py
@@ -50,3 +50,22 @@ def process_coins():
     total += int(input("how many nickles?: ")) * 0.05
     total += int(input("how many pennies?: ")) * 0.01
     return total
+
+def is_transaction_successful(money_received, drink_cost):
+    """Returns True when the payment is successful; False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money :( Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the available resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
