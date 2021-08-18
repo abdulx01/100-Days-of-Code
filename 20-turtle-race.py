ebb88dd41f9d6b5854672d28758a67f455fb56fb
Bala Priya <47279635+balapriyac@users.noreply.github.com>
2021-08-18 14:11:42 +0530

    Update 20-turtle-race.py

--- 20-turtle-race.py
+++ 20-turtle-race.py
@@ -1,1 +1,44 @@
+from turtle import Turtle, Screen
+import random
 
+# wait for user input to start the race
+is_race_on = False
+
+# set the screen size
+screen = Screen()
+screen.setup(width=500, height=400)
+
+# create user prompt
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+# Create 6 turtles
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        # 230 is 250 - half the width of the turtle
+        # end the race when the first turtle crosses the finish line
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        #Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
