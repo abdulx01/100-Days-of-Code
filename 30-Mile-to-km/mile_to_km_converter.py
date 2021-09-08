e368cb7cf50d523b1c250b2f83832c1498f0746d
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-09-08 14:11:52 +0530

    Added reqd. widgets

--- 30-Mile-to-km/mile_to_km_converter.py
+++ 30-Mile-to-km/mile_to_km_converter.py
@@ -3,6 +3,16 @@ from tkinter import *
 window = Tk()
 window.title("Miles to Kilometers Converter")
 
+# add widgets to screen
+miles_input = Entry()
+
+miles_label = Label(text="Miles")
+is_equal_label = Label(text="is equal to")
+km_result_label = Label(text="0")
+km_label = Label(text="km")
+
+calculate_button= Button(text="calculate")
+
 
 
 window.mainloop()
