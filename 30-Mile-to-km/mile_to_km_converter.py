c43d326cee328cb7d8dba3607a7b8f2a5bf34ed0
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-09-08 14:16:07 +0530

    lay out widgets on grid

--- 30-Mile-to-km/mile_to_km_converter.py
+++ 30-Mile-to-km/mile_to_km_converter.py
@@ -5,14 +5,22 @@ window.title("Miles to Kilometers Converter")
 
 # add widgets to screen
 miles_input = Entry()
+miles_input.grid(column=1,row=0)
 
 miles_label = Label(text="Miles")
+miles_label.grid(column=2,row=0)
+
 is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0,row=1)
+
 km_result_label = Label(text="0")
+km_result_label.grid(column=1,row=1)
+
 km_label = Label(text="km")
+km_label.grid(column=2,row=1)
 
-calculate_button= Button(text="calculate")
-
+calculate_button = Button(text="calculate")
+calculate_button.grid(column=1,row=2)
 
 
 window.mainloop()
