92b90eca6adb111e3dbf4060ded4af6156e3f435
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-09-13 17:24:37 +0530

    Added countdown

--- 31-Pomodoro Timer/main.py
+++ 31-Pomodoro Timer/main.py
@@ -43,6 +43,27 @@ def start_timer():
         title_label.config(text="Work", fg=GREEN)
         
         
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+        
+        
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
