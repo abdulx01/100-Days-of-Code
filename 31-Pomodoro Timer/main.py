5209da4ef6a16fa8ab1159dbde6bb29359dbc756
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2021-09-13 17:23:29 +0530

    Added TIMER RESET part

--- 31-Pomodoro Timer/main.py
+++ 31-Pomodoro Timer/main.py
@@ -12,7 +12,15 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
+# ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
